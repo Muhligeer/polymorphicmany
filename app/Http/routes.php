5d5bdfd76aa81b550cfb7768003dcc6486bdba5b
Muhligeer <guilhermejosecruz@gmail.com>
2019-03-08 11:36:29 -0300

    commit

---
+++ app/Http/routes.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+use App\Post;
+use App\Tag;
+use App\Video;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/create', function(){
+
+    $post = Post::create(['name'=>'first post']);
+
+    $tag1 = Tag::find(1);
+
+    $post->tags()->save($tag1);
+
+    $video = Video::create(['name'=>'first video']);
+
+    $tag2 = Tag::find(2);
+
+    $video->tags()->save($tag2);
+
+});
---
+++ app/Video.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    //
+
+    public function tags(){
+
+        return $this->morphToMany('App\Tag', 'taggable');
+
+    }
+
+}
